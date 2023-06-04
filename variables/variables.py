2150614e3a5fcd64fe24f6bb8c631a3049870d7f
jacoder <mantle@vivaldi.net>
2023-06-04 08:47:38 -0500

    VARIABLES
    Defining variables in python.

---
+++ variables/variables.py
@@ -0,0 +1,94 @@
+"""
+     variables are a way of storing data/information that I may want to keep for later.
+     A variable is general a placeholder for a value.
+     In python I dont have to tell the computer what type I want my variable to be, It
+     figures the type out automatically.
+"""
+
+# Global variables - defined outside the scope of any function
+# so I can access them from anywhere.
+# so the variables declared below out side a function are all global.
+
+#*** 1 => Creating and assigning variables
+# integer variables
+one = 1
+two = 2
+three = 3
+four = 4
+
+#*** 2 => use the print() function to print the variable values to the screen
+print('one: ', one)
+print('two: ', two)
+print('three ', three)
+two = 4         # over write value of variable two
+print('two: ', two)      # now variable two has new value of 4
+print('one:', one)      # re-use variable one
+print()
+
+#*** 3 => Creating variables of a different type - Decimal and Strings
+# decimal/float variable
+Decimal = 1.1
+
+# string variable - concatenation
+StringVar = "Hello" + "1"
+
+
+print('Decimal: ', Decimal)
+print('StringVar: ', StringVar)
+
+
+
+#*** 4 => Creating a Constant (in global scope)
+# constant in ALL CAPS
+PI = 3.14
+print('PI: ', PI)
+print()
+
+
+#*** 5 => Creating local variable within scope of a function
+#         these variables are not accessible outside the function
+def FunctionName():
+    global one  # specify global variable in local scope
+    # could also just remove from within function
+    # and just access directly print(one) since one is a global variable
+    newVar = "World" # local variable newVar, can't access outside function
+    print('global variable in function: ', one)
+    print('local variable in function: ', newVar)
+    return
+
+# call/run function
+FunctionName()
+# print(newVar) # when active this yields an error as it is not in global scope
+
+
+
+#*** 6 => shorthand way/notation of creating a variable
+four, five, six = 4, 5, 6 # four = 4; five = 5; six = 6
+print()
+print('four: ', four)
+print('five: ', five)
+print('six: ', six)
+
+
+
+
+
+#*** 7 => create a variable based on the sum of two previous variables
+ten = 5 + 5
+print('ten: ', ten)
+
+
+
+#*** 8 => Creating counting variables
+count = 0
+print(count)
+count += 1 # OR, count = count + 1
+print(count)
+count += 1
+print(count)
+
+
+# other shorthand versions for other numerical operators
+count -= 2 # count = count - 2
+count *= 3 # count = count * 3
+count /= 3 # count = count / 3
---
+++ variables/variables2.py
@@ -0,0 +1,92 @@
+"""
+    Watch Over Me Song Details.
+    Script outlining the details of one of my favourite
+    songs using Python variables.
+"""
+
+# variable declarations
+
+Title = "Watch Over Me"         # official name of the song
+Artist = "Jermaine Edwards"     # name of singer who sang the song
+Author = "Jermaine Edwards"     # the song writer
+Genre = "Reggae Gospel"         # category the style of music the song falls under
+Riddim = "watch"                # beat used to record song
+Producer = "Romeich Entertainment & Island worship Productions" # Groups which sponsor the song
+VideoProduction = "Iceyjace Films"  # Procucer/Director of the song's associated music video
+Album = "Watch Over Me"         # Album/Disk song was released on
+ReleaseYear = 2020              # Year song was released to the general public
+Duration = 151                  # lenght of play of song in seconds
+BeatMode = "Fast"               # pace of the song's beat
+Cost = 2550.55                  # Price for one recording of the song
+SaleCost = 1550.50              # Price of song whenever it is place on sale
+Currency = "JMD"                # monetary unit for the cost of the song
+Codec = "MPEG-1 Layer 3 (MP3)"  # encryption mode of song
+Channel = 'Stereo'              # song audio playback format
+SampleRate = 48000              # number of sample beats in song in Hz
+PlayCount = 2346                # Total amount of time I have played song this week
+FeaturesCount = 0               # number of additional artists on song
+Features = "none"               # names of additional artists on song
+CountryOfOrigin = "Jamaica"     # country which song is native of
+StarRatings = 4.5               # public rating of song from a total of five stars
+
+CopiesSold = 1254153            # total amount of copies of the song sold at regular cost,
+                                            # NOT ON SALE
+Revenue    = Cost * CopiesSold  # total monies gain from regular sales of the song
+
+SaleCopiesSold = 2457861        # total amount of copies of the song sold on sale
+
+SaleRevenue = SaleCost * SaleCopiesSold # total monies gain from SALE sales of the song
+
+TotalRevenue = Revenue + SaleRevenue    # total earnings of song as at current date
+
+
+
+
+"""
+    variable print outs - labels are attached to the actual variable
+    values for ease of read once printed out.
+"""
+print('Title: ', Title)
+print('Artist: ', Artist)
+print('Author: ', Author)
+print('Genre: ', Genre)
+print('Riddim: ', Riddim)
+print('Producer: ', Producer)
+print('Video Production: ', VideoProduction)
+print('Album: ', Album)
+print('Release Year: ', ReleaseYear)
+print('Duration (secs): ', Duration)
+print('Beat Mode: ', BeatMode)
+print('Cost: ', Cost)
+print('SaleCost: ', SaleCost)
+print('Currency: ', Currency)
+print('Codec: ', Codec)
+print('Channel: ', Channel)
+print('Sample Rate (Hz): ', SampleRate)
+print('play Count: ', PlayCount)
+print('Feature Count: ', FeaturesCount)
+print('Features: ', Features)
+print('Country of Origin: ', CountryOfOrigin)
+print('Star Ratings: ', StarRatings)
+
+print('Copies Sold: ', CopiesSold)
+print('Revenue: ', Currency + f"{Revenue: ,}")      # formatting number to String for better display
+                                                                # of figure, thus able to concatenate Currency value
+                                                                # which is a String type as well.
+
+print('Sale Copies Sold: ', SaleCopiesSold)
+print('Sale Revenue: ', Currency + f"{SaleRevenue: ,}")    # formatting number for better display
+
+
+print('Total Revenue: ', Currency + f"{TotalRevenue: ,}")  # formatting number for better display
+
+
+
+
+
+
+
+
+
+
+
