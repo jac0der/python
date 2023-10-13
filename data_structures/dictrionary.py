400ed60f0765d20f1157bdd97f70327dd5243e5f
jacoder <mantle@vivaldi.net>
2023-10-13 04:25:53 -0500

    DATA STRUCTURES - DICTIONARY
    Create python file for exploring the dictionary data structure.
    Defined 2 dictionries and added new key-value pairs to the dictionaries.

---
+++ data_structures/dictrionary.py
@@ -0,0 +1,64 @@
+'''
+    A dictionary is a collection of key-value pairs.
+    Each key is connected to a value by a colon (:),
+    and I can use a key to access the value associated with that key.
+
+    A dictionary is used to represent/connect related pieces of data for objects.
+    such as: List of words and their meanings
+             List of people names and their favorite numbers
+             List of mountains and their elevations etc...
+'''
+
+#*** 1 => Creating a dictionary
+
+jamaican_culture = {
+    'bird': 'doctor bird',
+    'fruit': 'ackee',
+    'dish': 'ackee and saltfish',
+    'tree': 'blue mahoe',
+    'mountain': 'blue mountain',
+    'river': "dunn's river",
+    'reggae': 'bob marley',
+    'sports': 'usain bolt'
+}
+print(jamaican_culture)
+
+student_average = {
+    'phil': 55,
+    'sarah': 70,
+    'edward': 65,
+    'jen': 95
+}
+print(student_average, '\n')
+
+
+#*** 2 => Add a new key-value pair
+jamaican_culture['hero'] = 'marcus garvey'
+
+student_average['esther'] = 98
+student_average['ruby'] = 49
+
+print(jamaican_culture)
+print(student_average, '\n')
+
+#*** 3 => 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
