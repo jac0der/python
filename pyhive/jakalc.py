ecc95632a78b5c41b6c48f520c0b7dc1a84e8cd1
jacoder <mantle@vivaldi.net>
2023-10-16 09:40:45 -0500

    PYTHON CALCULATOR Create a basic python calculator to perform basic mathematical operations.
    The eval() function was used to evaluate a mathematic equation
    entered by user.
    
    The equation was stripped of letters and special
    characters using the re python regex module to ensure the eval()
    function doesnt evaluate other valid python code entered by a user.

---
+++ pyhive/jakalc.py
@@ -0,0 +1,45 @@
+'''
+    Simple calculator to evaluate basic mathematic operations.
+    @datetime:: October 16, 2023 9:18 am (UTC-5)
+    @author:: jac0der
+'''
+# import regex module
+import re
+
+print("Jac0der Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def Calculate():
+    # access global variables within local scope of function Calculate()
+    global run
+    global previous
+
+    # initialize equation to empty string
+    equation = ""
+
+    # first calculation attempt
+    if previous == 0:
+
+        equation = input("Enter equation:")
+
+    else:
+
+        equation = input('~ ' + str(previous) + ' ')
+
+    if equation == 'quit':
+        print("Goodbye... Have a nice day!")
+        run = False
+    else:
+        # removing letters and special characters from user input
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+while run:
+    Calculate()
