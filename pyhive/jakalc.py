6dbb3eb6874da72697cadf889a4223090c62e77c
jacoder <mantle@vivaldi.net>
2023-10-16 10:33:23 -0500

    clean up calculator code

--- pyhive/jakalc.py
+++ pyhive/jakalc.py
@@ -5,9 +5,11 @@
 '''
 # import regex module
 import re
+import numpy as np
 
 print("Jac0der Calculator")
 print("Type 'quit' to exit\n")
+
 
 previous = 0
 run = True
@@ -23,22 +25,27 @@ def Calculate():
     # first calculation attempt
     if previous == 0:
 
-        equation = input("Enter equation:")
+        equation =  input("Enter equation:")
 
     else:
-
         equation = input('~ ' + str(previous) + ' ')
 
+
     if equation == 'quit':
+
         print("Goodbye... Have a nice day!")
         run = False
+
     else:
         # removing letters and special characters from user input
-        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+        equation = re.sub('[a-bd-zA-Z,.:()" "]', '', equation)
 
         if previous == 0:
+
             previous = eval(equation)
+
         else:
+
             previous = eval(str(previous) + equation)
 
 # loop to control the continous running of the calculator
