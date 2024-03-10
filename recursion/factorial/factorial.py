837e57627729759f5a3924311ca741072733d963
jacoder <mantle@vivaldi.net>
2024-03-10 01:39:08 -0500

    FACTORIAL Add main() method and use documentation strings for functions

--- recursion/factorial/factorial.py
+++ recursion/factorial/factorial.py
@@ -5,15 +5,21 @@
     @author:: jac0der
 """
 
-# getting number from user - retrieved as a string from user.
-str_number = input("Enter number: ")
 
-"""
-    Function to find the factorial of a number
-    @input:: number to retrieve factorial for
-    @output:: factorial of input number
-"""
+def main():
+    # getting number from user - retrieved as a string from user.
+    str_number = input("Enter number: ")
+
+    # call to valudate the input to ensure its a number
+    validateInput(str_number)
+
+
 def factorial(number):
+    """
+        Function to find the factorial of a number
+        @input:: number to retrieve factorial for
+        @output:: factorial of input number
+    """
 
     # 1! is 1 so return 1 - base case met
     if number == 1:
@@ -22,13 +28,13 @@ def factorial(number):
     # recursively call factorial on n - 1
     return number * factorial(number - 1)
 
-"""
-    Function to ensure user input is a valid integer
-    @input:: number to validate and get factorial for
-    @output:: message with factorial result for input number.
-"""
-def validateInput(value):
 
+def validateInput(value):
+    """
+        Function to ensure user input is a valid integer
+        @input:: number to validate and get factorial for
+        @output:: message with factorial result for input number.
+    """
     try:
         # try cast input to integer
         number = int(value)
@@ -39,5 +45,6 @@ def validateInput(value):
     except ValueError:
         print("Invalid Input...")       
 
-# call to valudate the input to ensure its a number
-validateInput(str_number)
+
+if __name__ == "__main__":
+    main()
