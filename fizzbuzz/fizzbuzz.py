d327ff382a923d60f4f1c885d144248944f661a7
jacoder <mantle@vivaldi.net>
2024-03-10 01:29:02 -0500

    FIZZBUZZ add documentation strings (doc strings) to the fizzbuzz program

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -16,35 +16,38 @@ def main():
     fizzbuzz()
 
 
-"""
-    Function to determine if a divisor is a multiple of dividend.
-    Used the modulo (%) operator to determine if divisor is a multiple
-    of dividend. Modulo operator returns the remainder from a division,
-    if zero (0) is returned, then divisor is a multiple of dividend.
-    @inputs::
-            dividend - number being divided
-            divisor - number being check to see if it is a multiple
-                      of dividend.
-    @output:: Boolean - True if divisor is a multiple of dividend,
-              otherwise, False.
-"""
 def is_multiple(dividend, divisor):
+    """
+        Function to determine if a divisor is a multiple of dividend.
+        Used the modulo (%) operator to determine if divisor is a multiple
+        of dividend. Modulo operator returns the remainder from a division,
+        if zero (0) is returned, then divisor is a multiple of dividend.
+        @inputs::
+                dividend - number being divided
+                divisor - number being check to see if it is a multiple
+                        of dividend.
+        @output:: Boolean - True if divisor is a multiple of dividend,
+                otherwise, False.
+    """
     if (dividend % divisor) == 0:
         return True
     else:
         return False
 
 
-"""
-    Function to loop through numbers from 1 to 100,
-    printing 
-        - FizzBuzz if a number is both a multiple of 3 and 5
-        - Fizz if a number is a multiple of 3
-        - Buzz if a number is a multiple of 5
-
-    The range() method is used to produce the numbers from 1 - 100.
-"""
 def fizzbuzz():
+    """
+        Function to loop through numbers from 1 to 100,
+        printing 
+            - FizzBuzz if a number is both a multiple of 3 and 5
+            - Fizz if a number is a multiple of 3
+            - Buzz if a number is a multiple of 5
+
+        The range() method is used to produce the numbers from 1 - 100.
+        @input:: none
+        @output:: none
+    """
+    
     # range method goes up to but not including the last index specified, so 
     # why 101 is specified, in dorder to include 100.
     for number in range(1, 101):
