b60485e1bc2fc811248d90ef685dca93f8c6d4ab
jacoder <mantle@vivaldi.net>
2024-03-13 23:14:29 -0500

    ROCK PAPER SISSORS Add comments for the get_rounds function

--- pyprojects/rock_paper_scissors/rps.py
+++ pyprojects/rock_paper_scissors/rps.py
@@ -20,18 +20,22 @@ def main():
 
 def get_rounds():
     '''
-        function to get the user's input for the number 
-        of rounds of rock paper sissors to play.
+        Function to get the user's input for the number 
+        of rounds of rock paper sissors to play. 
+        User's input is also validated to prevent errors.
 
         @input::  none
         @output::int -> user's entered number of rounds to play
     '''
 
+    # loop as long as an invalid input is entered by user.
     while(True):
 
         try:
+            # try casts users input to integer
             rounds = int(input("Number of rounds: "))
 
+            # only accepts positive whole numbers
             if rounds > 0:
                 return rounds
             else:
@@ -41,7 +45,6 @@ def get_rounds():
             print("Invalid Input, Please enter a numberic value.")
 
 
-# validate user input
 
 # get random computer choice
 
