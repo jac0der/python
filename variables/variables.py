15f2837d5209c65a176ba00f5f9b91dee9d4a111
jacoder <mantle@vivaldi.net>
2024-08-18 04:19:43 -0500

    VARIABLES - REFACTOR
    removed function vcariable scope from variables. moved to separate
    explanation of variable scope file.

--- variables/variables.py
+++ variables/variables.py
@@ -17,6 +17,7 @@ two = 2
 three = 3
 four = 4
 
+
 #*** 2 => use the print() function to print the variable values to the screen
 print('one: ', one)
 print('two: ', two)
@@ -25,6 +26,7 @@ two = 4         # over write value of variable two
 print('two: ', two)      # now variable two has new value of 4
 print('one:', one)      # re-use variable one
 print()
+
 
 #*** 3 => Creating variables of a different type - Decimal and Strings
 # decimal/float variable
@@ -38,7 +40,6 @@ print('Decimal: ', Decimal)
 print('StringVar: ', StringVar)
 
 
-
 #*** 4 => Creating a Constant (in global scope)
 # constant in ALL CAPS
 PI = 3.14
@@ -46,24 +47,7 @@ print('PI: ', PI)
 print()
 
 
-#*** 5 => Creating local variable within scope of a function
-#         these variables are not accessible outside the function
-def FunctionName():
-    global one  # specify global variable in local scope
-    # could also just remove from within function
-    # and just access directly print(one) since one is a global variable
-    newVar = "World" # local variable newVar, can't access outside function
-    print('global variable in function: ', one)
-    print('local variable in function: ', newVar)
-    return
-
-# call/run function
-FunctionName()
-# print(newVar) # when active this yields an error as it is not in global scope
-
-
-
-#*** 6 => shorthand way/notation of creating a variable
+#*** 5 => shorthand way/notation of creating multiple variable with different values
 four, five, six = 4, 5, 6 # four = 4; five = 5; six = 6
 print()
 print('four: ', four)
@@ -71,13 +55,14 @@ print('five: ', five)
 print('six: ', six)
 
 
-
+#*** 6 => horthand way/notation of creating variables with same value
+num1=num2=num3 = 10
+print("same values", num1, num2, num3)
 
 
 #*** 7 => create a variable based on the sum of two previous variables
 ten = 5 + 5
 print('ten: ', ten)
-
 
 
 #*** 8 => Creating counting variables
@@ -90,6 +75,7 @@ print(count)
 
 
 # other shorthand versions for other numerical operators
+# variable++ in python
 count -= 2 # count = count - 2
 count *= 3 # count = count * 3
 count /= 3 # count = count / 3
