b34ba314d1dba42f637d00be070fa474fc56439a
jacoder <mantle@vivaldi.net>
2024-08-22 23:44:47 -0500

    PYGAME - EXPLORE SPRITES
    load an image into a surface to be used as a sprite after inheriting
    from the pygame.sprite.Sprite class.

---
+++ pygame/sprites/character/character.py
@@ -0,0 +1,58 @@
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+)
+
+# Define a Character object by extending pygame.sprite.Sprite
+# Instead of a surface, use an image for a better-looking sprite
+class Character(pygame.sprite.Sprite):
+    
+    def __init__(self):
+
+       # self.surface = pygame.Surface( (50, 50) )
+       # self.surface.fill( (0, 0, 0) )
+
+       '''
+            pygame.image.load() loads an image from the disk. 
+            You pass it a path to the file. It returns a Surface, 
+            and the .convert() call optimizes the Surface, making 
+            future .blit() calls faster.
+            RLEACCEL - removes white background, make sprite thave transparency around it.
+       '''
+       self.surface = pygame.image.load("./assets/jet.png").convert()
+       self.surface.set_colorkey((255, 255, 255), RLEACCEL)
+       self.rect = self.surface.get_rect()
+
+    # Move the Player object based on user keypresses
+    def update(self, pressed_keys, screen_width, screen_height):
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
--- pygame/sprites/sprite.py
+++ pygame/sprites/sprite.py
@@ -1,6 +1,8 @@
 #import pygame
 import pygame
 
+import character.character as c
+
 # Import pygame.locals for easier access to key coordinates
 from pygame.locals import (
     K_ESCAPE,
@@ -19,7 +21,9 @@ pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # set screen title
-pygame.display.set_caption("Blit Surfaces OOP")
+pygame.display.set_caption("Using Sprite")
+
+character = c.Character()
 
 # setup game loop
 running = True
@@ -39,6 +43,18 @@ while running:
 
             if event.key == K_ESCAPE:
                 running = Fasle
+
+    # get dictonary of input keys where each key has a value of True or False. 
+    # Where True is pressed and Fasle is not pressed.
+    keys_pressed = pygame.key.get_pressed()
+
+    character.update(keys_pressed, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    # fill screen
+    screen.fill( (124, 189, 215) )
+
+    # blit player to screen at current location of the rectangle surface
+    screen.blit(character.surface, character.rect) 
 
     pygame.display.flip()
 
