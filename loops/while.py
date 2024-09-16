247e6071c8cf36c1e93b91c7d1566baa6ada327c
jacoder <mantle@vivaldi.net>
2024-09-15 23:38:49 -0500

    LOOPS
    Exploring python while loop.

---
+++ loops/while.py
@@ -0,0 +1,13 @@
+'''
+    A while loop is used to perform an action 
+    as long as a specified condition is true.
+
+    @datetime:: September 15, 2024 11:35 pm (UTC-5)
+    @author:: jac0der
+'''
+
+x = 1
+
+while x < 10:
+    print(f'x is: {x}')
+    x += 1 # increment value of x to prevent infinite loop
