2ebd8a8b411e78e28f6adca0166be1a6a4fb659a
jacoder <mantle@vivaldi.net>
2024-09-15 23:47:20 -0500

    LOOPS
    Add loop to accept user input and reprompt for user input if
    entered number is not betwen 4 and 8 inclusive.

--- loops/while.py
+++ loops/while.py
@@ -6,8 +6,21 @@
     @author:: jac0der
 '''
 
+
 x = 1
 
 while x < 10:
     print(f'x is: {x}')
     x += 1 # increment value of x to prevent infinite loop
+
+
+try:
+    number = int(input("Enter a number: "))
+
+    while(number < 4 or number > 8):
+        number = int(input("Enter a number: "))
+
+    print(f'success number is: {number}')
+
+except Exception:
+    print('Invalid input, please enter a number.')
