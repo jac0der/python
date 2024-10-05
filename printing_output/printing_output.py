f4586ad324cafbd638cef2a160c3e61f784cdc08
jacoder <mantle@vivaldi.net>
2024-10-04 22:18:05 -0500

    PRINT FUNCTION
    use the sep= parameter to separate printed objects.

--- printing_output/printing_output.py
+++ printing_output/printing_output.py
@@ -4,13 +4,16 @@
     @datetime:: October 02, 2024 11:03 pm (UTC-5)
     @author:: jac0der
 '''
+# syntax => print(object= separator= end= file= flush=)
+
+
 #*** 1 => simple printing
 print("Welcome to jacoder coding!")
 print("Welcome to Jamrock!")
 
 
 #*** 2 => concatenating strings
-print("God is", "a Good", "God") # dont have to space when using commas
+print("God is", "a Good", "God") # dont have to space when using commas, items spaced automatically
 print("Well done " + "thou good and" + " faithful servant")
 
 # using f-string
@@ -20,7 +23,30 @@ hobby = "reading"
 print(f"{fruit} is my favorite fruit.")
 
 # using string.format()
-thningsme = "I enjoyed playing {0} growing up as a child, but I also like {1} after school.".format(sport, hobby)
+# I can specify the order in which they are printed by using numbers (tuple index)
+thningsme = "I enjoyed playing {} growing up as a child, but I also like {} after school.".format(sport, hobby)
 print(thningsme)
 
-print("hello", file='testtt')
+
+#*** 3 => print() with the end= parameter
+# newline at end of print
+print("This is the first line.", end='\n')
+print("New line print starts here.\n")
+
+# tab at end of print
+print("This is a tab test.", end='\t\t')
+print("After tab at end.")
+
+
+#*** 4 => print() with the sep= parameter
+print("November 5", "is the biggest election", "for the USA.", "asdfg",sep=' | ')
+
+
+
+
+# Code for printing to a file
+#sample = open('samplefile.txt', 'w')
+ 
+#print('Print to a file', file = sample)
+#sample.close()
+
