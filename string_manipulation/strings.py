a2670aa3f3eeca16b9a670e1dfe37a396a4495e2
jacoder <mantle@vivaldi.net>
2024-10-04 22:46:35 -0500

    STRING MANIPULATION
    use + operator for concatenation. make a string lowercase, uppercase.
    trim excess spaces from variable.

--- string_manipulation/strings.py
+++ string_manipulation/strings.py
@@ -4,3 +4,36 @@
     @datetime:: October 04, 2024 10:28 pm (UTC-5)
     @author:: jac0der
 '''
+
+
+#*** 1 => creating a string using both double and single quotes
+country = "Barain"
+city = 'Barcelonia'
+print(country, city, sep=' ', end='\n\n')
+
+
+#*** 2 => concatenation (using the + operator)
+print(country + " " + city)
+
+
+#*** 3 =>  lower case
+title = "The Great Commission"
+print(title.lower())
+
+
+#*** 4 => upper case
+print(title.upper())
+
+
+#*** 5 =>  trimming
+language = '   Cobalt      '
+print(language)
+
+# trim both sides
+print(language.strip())
+
+# trim left side
+print(language.lstrip())
+
+# print right side
+print(language.rstrip())
