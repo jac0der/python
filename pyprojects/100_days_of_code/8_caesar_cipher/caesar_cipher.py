e75a294ac177a41b31e06f334465162d55c03327
jacoder <mantle@vivaldi.net>
2024-12-08 18:12:40 -0500

    CAESAR CIPHER
    refactor code to only use one methoth caesar() to perform both
    the caersar encrypt and decrypt based on the operation passed in to be
    performed.

--- pyprojects/100_days_of_code/8_caesar_cipher/caesar_cipher.py
+++ pyprojects/100_days_of_code/8_caesar_cipher/caesar_cipher.py
@@ -24,15 +24,26 @@ def main():
 
     try:
         shift = int(input("Type the shift number:\n"))
-        
+
     except ValueError:
-        print("Invalid shift amount entered.")
+        print("Invalid shift amount entered. Only Numeric values allowed.")
+        return 0
+
+    if direction not in ('encode', 'decode'):
+        print('Invalid operation option entered.')
+        return 0
+
+    if text == '':
+        print('No text entered to encrypt/decrypt.')
         return 0
 
     response = caesar(direction, text, shift)
 
-    print(response)
+    if response == '':
+        print("Error occured.")
+        return 0
 
+    print(f'The {direction}d value is: {response}')
 
 
 def caesar(direction, original_text, shift_amount):
@@ -46,8 +57,42 @@ def caesar(direction, original_text, shift_amount):
                   shift_amounbt-> int: the amount of shifts to be appied to original_text.
         
         @output:: message-> str: the encrypted caesar cipher by specified shift amount or
-                                 the plain_text decrypted cipher or an error message.
+                                 the plain_text decrypted cipher or an empty string indicating an error.
     '''
+    output_value = ''
+
+    try:
+        # set shift to be negative when decrypting
+        if direction == "decode":
+            shift_amount *= -1
+
+        for letter in original_text:
+
+            # if current letter is not actually a letter but a symbol or number...
+            # just add it back to the output value.
+            if letter not in ALPHABET:
+                output_value += letter
+                continue
+
+            # get the current index position for letter
+            index = ALPHABET.index(letter) 
+            shift_index = index + shift_amount
+
+            # check if the shift amount plus current index of letter out range of 25 (z)
+            # if so find the modulus of that number with length of ALPHABET list (26)
+            if shift_index >= len(ALPHABET):
+                shift_index %= len(ALPHABET) # ensure stay within range of 0-25
+
+            shift_letter = ALPHABET[shift_index]
+            output_value += shift_letter
+
+    except Exception:
+        # error occured during cipher generation, so log message and return empty string
+        return ''
+
+    return output_value
+
+'''
     message = ''
 
     if direction == "encode":
@@ -70,11 +115,11 @@ def caesar(direction, original_text, shift_amount):
         message = 'Invalid option entered.'
 
     return message
-
-
+'''
+'''
 # by the shift amount and print the encrypted text.
 def encrypt(original_text, shift_amount):
-    '''
+    \'''
         function to create a caesar cipher encrypted text based on a specified
         amount of shifts.
 
@@ -82,7 +127,7 @@ def encrypt(original_text, shift_amount):
                   shift_amounbt-> int: the amount of shifts to be appied to original_text.
         
         @output:: cipher-> str: the encrypted caesar cipher by specified shift amount.
-    '''
+    \'''
     cipher = ''
 
     try:
@@ -109,7 +154,7 @@ def encrypt(original_text, shift_amount):
 
 
 def decrypt(original_text, shift_amount):
-    '''
+    \'''
         function to create a caesar cipher decrypt function to decrypt caesar cipher
         encrypted texts based on a specified amount of shifts.
 
@@ -117,7 +162,7 @@ def decrypt(original_text, shift_amount):
                   shift_amounbt-> int: the amount of shifts to be appied to the cipher for decrypting.
         
         @output:: plain_text-> str: the decrypted plain text of the encrypted caesar cipher.
-    '''
+    \'''
     plain_text = ''
 
     try:
@@ -141,7 +186,7 @@ def decrypt(original_text, shift_amount):
         return ''
 
     return plain_text
-
+'''
 
 if __name__ == "__main__":
     main()
