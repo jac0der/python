1b92ecc3f4a0a3df5a66e82c97771f17b5b3067b
jacoder <mantle@vivaldi.net>
2025-01-29 06:38:31 -0500

    CALCULATOR
    Setup initial project for simple calculator app.

---
+++ pyprojects/100_days_of_code/10_calculator/calculator.py
@@ -0,0 +1,13 @@
+'''
+    Create a calculator app to perform basic mathematical operations.
+
+    @datetime:: January 29, 2025 6:37 am (UTC-5)
+    @author:: jacoder
+'''
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
