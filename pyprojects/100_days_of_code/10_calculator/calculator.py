2b23ecd6426eda245351fcae65e2f7b9ea902331
jacoder <mantle@vivaldi.net>
2025-02-02 19:48:07 -0500

    CALCULATOR
    Imported jaclog to insert logging statements to track and monitor
    calculator code.

--- pyprojects/100_days_of_code/10_calculator/calculator.py
+++ pyprojects/100_days_of_code/10_calculator/calculator.py
@@ -4,7 +4,12 @@
     @datetime:: January 29, 2025 6:37 am (UTC-5)
     @author:: jacoder
 '''
-import os, operator, art
+import os, sys, operator, art
+
+# Add the 'logging' folder to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../logging')))
+import jaclog
+logger = jaclog.configure('calculator_100days', './calculator.log')
 
 
 OPERATIONS = {
@@ -28,7 +33,7 @@ def get_number(prompt):
         try:
             return float(input(prompt))
         except ValueError:
-            print("Invalid number. Please enter a valid numeric value.")
+            logger.error('Invalid number entered for math calculation.' + '\n' + 'Please enter a valid numeric value.')
 
 
 def get_operation():
@@ -41,15 +46,19 @@ def get_operation():
         Returns: 
             str:  selected math operation.
     '''
-    print("\nAvailable operations:")
+    logger.info('Getting math operation.') 
+
+    print("\nAvailable Math operations:")
     for op in OPERATIONS:
         print(op)
 
     while True:
         operation = input("Pick an operation: ").strip()
         if operation in OPERATIONS:
+            logger.info(f'Selected operation is {operation}.')
             return operation
-        print("Invalid operation. Please choose from the list.")
+
+        logger.warning('Invalid operation. Please choose from the list of math operations.')
 
 
 def calculate(first_number, second_number, operation):
@@ -64,36 +73,42 @@ def calculate(first_number, second_number, operation):
                  float: The result of the math operation selected.
     '''
     try:
+        logger.info(f'Calculating {first_number} {operation} {second_number}') 
         return OPERATIONS[operation](first_number, second_number)
     except ZeroDivisionError as ex:
-        print(f"Error: Cannot divide {first_number} by {second_number}.")
-        print(f"Error details: {ex}")
+        logger.error(f'Error: Cannot divide {first_number} by {second_number}.')
+        logger.error(f'Error details: {ex}')        
         return None
     except Exception as ex:
-        print(f"An unexpected error occurred while performing '{first_number} {operation} {second_number}'.")
-        print(f"Error details: {ex}")
-        print("Please verify your inputs and try again.")
+        logger.error(f'An unexpected error occurred while calculating {first_number} {operation} {second_number}.')
+        logger.error(f'Error details: {ex}')  
+        logger.error('Please verify your inputs and try again.')
         return None
 
 
 def main():
     ''' main function to run the calculator'''
+    logger.info('Starting calculator.')
     print(art.logo)
     print("Welcome to the Calculator App!")
 
     try:
-        while True:           
+        while True:
+            logger.info('Getting first number.')           
             first_number = get_number("What's the first number?: ")
             continue_calculating = True         
 
             while continue_calculating:
-
+                
                 operation = get_operation()
+
+                logger.info('Getting second number.') 
                 second_number = get_number("What's the next number?: ")
                     
                 result = calculate(first_number, second_number, operation)
 
                 if result is not None:
+                    logger.info(f'Result is: {result}.')
                     print(f"{first_number} {operation} {second_number} = {round(result, 2)}")
 
                     # Continue or restart
@@ -106,10 +121,11 @@ def main():
                         os.system('cls||clear')  # Clear screen
                         continue_calculating = False
                 else:
+                    logger.warning(f'No result from calculation of {first_number} {operation} {second_number}.')
                     continue  # Skip result display if calculation failed
            
     except Exception as ex:
-        print("Error occured." + '\n' + str(ex))
+        logger.error("Error occured in main calculator function." + '\n' + str(ex))
 
 
 if __name__ == "__main__":
