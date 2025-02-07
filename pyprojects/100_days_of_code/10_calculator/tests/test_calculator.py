81d9f6f6b9549ff75620ed62e9df5fb9644ed61f
jacoder <mantle@vivaldi.net>
2025-02-07 02:19:14 -0500

    CALCULATOR - UNIT TEST
    Added valid test cases for operations multiplication, addition and
    subtraction.

--- pyprojects/100_days_of_code/10_calculator/tests/test_calculator.py
+++ pyprojects/100_days_of_code/10_calculator/tests/test_calculator.py
@@ -5,6 +5,16 @@ class TestCalculator(unittest.TestCase):
 
     def test_calculate_valid_inputs(self):         
         self.assertEqual(calculate(6.0, 2.0, '/'), 3.0)
+        self.assertEqual(calculate(3.0, 2.4, '/'), 1.25)
+        
+        self.assertEqual(calculate(100.3375, 2.733, '*'), 274.22)
+        self.assertEqual(calculate(94.95, 67.89, '*'), 6446.16)
+
+        self.assertEqual(calculate(-7.0, 2.0, '+'), -5.0)
+        self.assertEqual(calculate(-8.0, -2.4, '+'), -10.4)
+
+        self.assertEqual(calculate(7.3, 8.9, '-'), -1.6)
+        self.assertEqual(calculate(33.33, 4.0, '-'), 29.33)
 
 
 if __name__ == '__main__':
