aef90ef8fbb732dddcb9908ce6595d26b3c34750
jacoder <mantle@vivaldi.net>
2025-02-07 01:59:23 -0500

    CALCULATOR - UNIT TEST
    Add test case for valid inputs to the calculate function.

--- pyprojects/100_days_of_code/10_calculator/tests/test_calculator.py
+++ pyprojects/100_days_of_code/10_calculator/tests/test_calculator.py
@@ -3,24 +3,9 @@ from calculator.calculator import calculate, get_number, get_operation
 
 class TestCalculator(unittest.TestCase):
 
-    def test_addition(self):
-        self.assertEqual(calculate(10, 5, '+'), 15)
+    def test_calculate_valid_inputs(self):         
+        self.assertEqual(calculate(6.0, 2.0, '/'), 3.0)
 
-    def test_subtraction(self):
-        self.assertEqual(calculate(10, 5, '-'), 5)
-
-    def test_multiplication(self):
-        self.assertEqual(calculate(10, 5, '*'), 50)
-
-    def test_division(self):
-        self.assertEqual(calculate(10, 5, '/'), 2)
-
-    def test_division_by_zero(self):
-        self.assertIsNone(calculate(10, 0, '/'))
-
-    def test_invalid_operation(self):
-        self.assertIsNone(calculate(10, 5, '%'))
-       
 
 if __name__ == '__main__':
     unittest.main()
