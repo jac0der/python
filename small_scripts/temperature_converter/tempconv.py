d43f4d550cb84ee0f5e58a03b04932c39481f5be
jacoder <mantle@vivaldi.net>
2025-02-08 02:48:40 -0500

    TEMPERATURE CONVERTOR
    Added logging module to program.

--- small_scripts/temperature_converter/tempconv.py
+++ small_scripts/temperature_converter/tempconv.py
@@ -4,3 +4,22 @@
     @datetime:: February 8, 2025 2:42 am (UTC-5)
     @author:: jacoder
 '''
+import os 
+import sys
+
+# Add the 'logging' folder to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../logging')))
+import jaclog
+logger = jaclog.configure('temperature_convertor', './tempconv.log')
+
+
+def main():
+    try:
+        logger.info("Starting Temperature Converter.")
+        pass
+    except Exception as ex:
+        logger.exception("Error occured in main calculator function.")
+
+
+if __name__ == "__main__":
+    main()
