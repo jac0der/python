ad1f1a7f00e152a982351a52ae6b196b7dccc321
jacoder <mantle@vivaldi.net>
2025-02-08 23:22:02 -0500

    EVEN ODD
    Initial set up for the even odd program.

---
+++ small_scripts/even_odd/even_odd.py
@@ -0,0 +1,13 @@
+'''
+    Determin if a number is either an even or an odd number.
+
+    @datetime:: February 8, 2025 11:18 pm (UTC-5)
+    @author:: jac0der
+'''
+def main():
+    """ Main function to start Even Odd Program. """
+    pass
+
+
+if __name__ == "__name__":
+    main()
