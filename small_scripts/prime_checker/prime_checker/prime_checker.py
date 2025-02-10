9494fd848300ca130daf72b39c8891cc61a31401
jacoder <mantle@vivaldi.net>
2025-02-10 01:01:29 -0500

    PRIME CHECKER
    Added centralized logging.

--- small_scripts/prime_checker/prime_checker/prime_checker.py
+++ small_scripts/prime_checker/prime_checker/prime_checker.py
@@ -4,11 +4,19 @@
     @datetime:: February 10, 2025 12:56 am (UTC-5)
     @author:: jac0der
 '''
+import sys
+import os
+
+# Add the 'logging' folder to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../logging')))
+import jaclog
+logger = jaclog.configure('prime_checker', './prime_checker.log')
+
 
 def main():
     """ Main function to start the isPrime check Program. """
     try:
-        pass
+        logger.info('Starting the Prime Checker Program.')
 
     except Exception:
         pass
