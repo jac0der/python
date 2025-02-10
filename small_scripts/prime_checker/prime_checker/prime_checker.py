29b3464907598c2d0f36f6ad434334852d22569d
jacoder <mantle@vivaldi.net>
2025-02-10 00:59:00 -0500

    PRIME CHECKER
    Created initial is prime program structure.

---
+++ small_scripts/prime_checker/prime_checker/prime_checker.py
@@ -0,0 +1,18 @@
+'''
+    Check if a number is Prime.
+
+    @datetime:: February 10, 2025 12:56 am (UTC-5)
+    @author:: jac0der
+'''
+
+def main():
+    """ Main function to start the isPrime check Program. """
+    try:
+        pass
+
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    main()
