8ba0a7a0a89e6b15fa4e2a28138b5fd712d2134b
jacoder <mantle@vivaldi.net>
2025-02-22 23:05:02 -0500

    LIST REMOVE DUPLICATES Add process_duplicates function to remove duplicates from a list of list of items.

--- small_scripts/list_remove_duplicates/remove_dups/remove_dups.py
+++ small_scripts/list_remove_duplicates/remove_dups/remove_dups.py
@@ -6,33 +6,62 @@
 '''
 from logging_custom import jaclog
 import remove_dups_constants as rdc
+from typing import Any
 
 logger = jaclog.configure('list_remove_dups', './list_remove_dups.log')
 
 
-def remove_duplicates(list_items:list[any])->list[any]:
+def remove_duplicates(list_items:list[Any])->list[Any]:
     '''
-    Remove duplicate items from a list
+    Remove duplicate items from a list.
 
     Args:
-            list_items (list[any]): List of duplicated items.
+            list_items (list[any]): List of duplicated items, no specific data type.
     Returns:
             list[any]: List of unique items.
     '''
-    return list(set(list_items))
+    if not isinstance(list_items, list):
+        raise TypeError("Invalid Type: Expecting list for list_items parameter.")
+
+    logger.info("Removing duplicates from list.")
+    seen = set()
+    return [x for x in list_items if not (x in seen or seen.add(x))]
+
+
+def process_duplicates(collection:list[list[Any]])->None:
+    '''
+    Process a collection of lists, removing duplicates from each list within the
+    collection of lists.
+
+    Args:
+            collection (list[list[any]]): A collection of lists to remove duplicates from.
+    '''
+    if not isinstance(collection, list):
+        raise TypeError("Invalid Type: Expecting list for collection parameter.")
+
+    logger.info("Processing list collection.")
+
+    unique_lists = [f"{remove_duplicates(inner_list)}" for inner_list in collection]
+    result = "\n".join(map(str, (remove_duplicates(inner_list) for inner_list in collection)))
+    #result = "\n".join(unique_lists)
+    print(result)
 
 
 def main()->None:
     """ Main function to start the Remove Duplicates program """
     try:
         logger.info('Starting the remove dupliates program.')
+        process_duplicates(rdc.LIST_COLLECTION)
+
+    except TypeError as ex:
+        logger.error(f"TypeError: {ex}")
     
     except (KeyboardInterrupt, EOFError):
         print(f"\n{rdc.EXIT_MESSAGE}")
         return
 
     except Exception:
-        logger.exception("Error occurred in main Find Largest List Item function.")
+        logger.exception("Error occurred in main Remove Duplicates function.")
 
 
 if __name__ == "__main__":
