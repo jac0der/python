37794dd3508e71977c7ea1ef8c53ad8a81f4a7ee
jacoder <mantle@vivaldi.net>
2025-02-25 03:00:18 -0500

    NUMBER GUESS Added the get_random_number function to return a number between a specified range.

--- pyprojects/100_days_of_code/12_number_guesser/number_guess/number_guess.py
+++ pyprojects/100_days_of_code/12_number_guesser/number_guess/number_guess.py
@@ -6,10 +6,28 @@ Guess a number between 1 and 100.
 '''
 
 import art
+import random
 from logging_custom import jaclog
 import number_guess_constants as ngc
 
 logger = jaclog.configure('number_guess', './number_guess.log')
+
+
+def get_random_number(minimum:int, maximum:int)->int:
+    """
+    Generate a random number between a specified minimum and maximum values.
+
+    Args:
+            minimum (int): The lower bound value from which random number is selected.
+            maximum (int): The upper bound value from which random number is selected.
+    Returns:
+            int: The generated random number from the specified range.
+    """
+    if not isinstance(minimum, int) and not isinstance(maximum, int):
+        raise TypeError(f"Invalid Input: Expected Type int for both minimum {minimum} and maximum {maximum} imputs.")
+
+    logger.info(f"Getting the random number between {minimum} and {maximum}.")
+    return random.randint(minimum, maximum)
 
 
 def main()->None:
@@ -17,6 +35,11 @@ def main()->None:
     try:
         logger.info('Starting the Number Guessing program.')
         print(art.logo)
+
+        number = get_random_number(ngc.MINIMUM_GUESS, ngc.MAXIMUM_GUESS)
+
+    except TypeError as ex:
+        logger.error(f"TypeError: {ex}")
 
     except (KeyboardInterrupt, EOFError):
         print(f"\n{ngc.EXIT_MESSAGE}")
