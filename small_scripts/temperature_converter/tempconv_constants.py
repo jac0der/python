4fc7bd6ca33bf599e4cf10b085db3f92dd344724
jacoder <mantle@vivaldi.net>
2025-03-03 23:48:37 -0500

    TEMPERATURE CONVERTER Add type validations for the constants and enum type.

--- small_scripts/temperature_converter/tempconv_constants.py
+++ small_scripts/temperature_converter/tempconv_constants.py
@@ -1,7 +1,7 @@
 # connstants definitions
-EXIT_MESSAGE = 'Goodbye!'
-INVALID_INPUT = 'Invalid Input'
-TEMPERATURE_MESSAGE = 'Temperature entered is: {0}.'
-CONVERSION_TYPE_WARNING = 'Invalid conversion option "{0}" selected. Expecting 1 or 2.'
-GET_TEMPERATURE_WARNING = 'Invalid number entered for temperature.' + '\n' + 'Please enter a valid numeric value.'
-CONVERSION_ISSUE = 'Conversion Type Code Issue:'
+EXIT_MESSAGE:str = 'Goodbye!'
+INVALID_INPUT:str = 'Invalid Input'
+TEMPERATURE_MESSAGE:str = 'Temperature entered is: {0}.'
+CONVERSION_TYPE_WARNING:str = 'Invalid conversion option "{0}" selected. Expecting 1 or 2.'
+GET_TEMPERATURE_WARNING:str = 'Invalid number entered for temperature.' + '\n' + 'Please enter a valid numeric value.'
+CONVERSION_ISSUE:str = 'Conversion Type Code Issue:'
--- small_scripts/temperature_converter/tempconv_enum.py
+++ small_scripts/temperature_converter/tempconv_enum.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
 class ConversionType(Enum):
-    CELSIUS_TO_FAHRENHEIT = "CTF"
-    FAHRENHEIT_TO_CELSIUS = "FTC"
+    CELSIUS_TO_FAHRENHEIT:str = "CTF"
+    FAHRENHEIT_TO_CELSIUS:str = "FTC"
