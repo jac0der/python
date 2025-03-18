9387f3dc9d78b7a228fa221390d2e36cfd0fd010
jacoder <mantle@vivaldi.net>
2025-03-17 22:17:40 -0500

    COFFEE MACHINE Add display_logo function to print the coffe logo art at start of program.

--- pyprojects/100_days_of_code/15_coffee_machine/coffee/coffee_machine.py
+++ pyprojects/100_days_of_code/15_coffee_machine/coffee/coffee_machine.py
@@ -6,17 +6,25 @@ coffee for customers.
 @author:: jac0der
 '''
 
-import coffee_machine_constants as cmc
+from art import logo
 import coffee_data as cd
 from logging_custom import jaclog
+import coffee_machine_constants as cmc
 
 logger = jaclog.configure('coffee_machine', './coffee_machine.log')
+
+
+def display_logo()->None:
+    """ Prints the logo for the Coffee Machine program. """
+    logger.info("Printing coffee logo art.")
+    print(logo)
 
 
 def main()->None:
     """ Main function to start the Coffee Machine Program. """
     try:
         logger.info("Starting the coffee machine program.")
+        display_logo()
     
     except (KeyboardInterrupt, EOFError):
         print(f"\n{cmc.EXIT_MESSAGE}")
