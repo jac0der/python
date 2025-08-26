f1262f3d46a306cc99f8143c1a1c963b1ffcf656
jacoder <mantle@vivaldi.net>
2025-08-25 22:00:57 -0500

    BAND GENERATOR
    Add exit_program method and try except for main method.

--- pyprojects/100_days_of_code/1_band_name_generator/bandname.py
+++ pyprojects/100_days_of_code/1_band_name_generator/bandname.py
@@ -5,6 +5,20 @@
     @datetime:: September 17, 2024 9:00 am (UTC-5)
     @author:: jacoder
 '''
+import sys
+
+EXIT_MESSAGE:str = 'Goodbye!'
+
+def exit_program(message:str, code:int=0)->None:
+    '''
+    Centralized exit function to handle the program termination.
+
+    Args:
+            message (str): Message to display and log when exiting.
+            code (int): Exit code (0 for normal exit, 1 for errors).
+    '''
+    print(message)
+    sys.exit(code)
 
 
 def generate_band_name()->str:
@@ -14,16 +28,21 @@ def generate_band_name()->str:
         @input:: none
         @output:: str - name of band
     '''
-    print('Welcome to the Band Name Generator.')
+    try:
+        print('Welcome to the Band Name Generator.')
 
-    city = input("What's the name of the city you grew up in?\n")
-    pet = input("What's your pets's name?\n")
+        city = input("What's the name of the city you grew up in?\n")
+        pet = input("What's your pets's name?\n")
 
-    band_name = f"Your band name could be {city + ' ' + pet}"
-    return band_name
+        band_name = f"Your band name could be {city + ' ' + pet}"
+        return band_name
+    
+    except (KeyboardInterrupt, EOFError):
+        exit_program("\n"+EXIT_MESSAGE)
 
 
 def main()->None:
+    """ Main function to start the Band Generator program. """
     print(generate_band_name())
 
 
