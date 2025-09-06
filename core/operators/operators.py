2bf05af30b86d6ffb7f009a515984d105ac098fb
jacoder <mantle@vivaldi.net>
2025-09-06 10:04:20 -0500

    OPERATORS - ARITHMETIC
    use python's aithmetic operators.

---
+++ core/operators/operators.py
@@ -0,0 +1,17 @@
+'''
+    Exploring operators in python.
+
+    @datetime:: September 06, 2025 9:54 am (UTC-5)
+    @author:: jac0der
+'''
+
+#*** 1 => arithmetic operators
+x,y = 8,2
+
+print(f'sum of {x} and {y}: {x + y}')               # 10
+print(f'difference of {x} and {y}: {x - y}')        # 6    
+print(f'product of {x} and {y}: {x * y}')           # 16
+print(f'division of {x} and {y}: {x / y}')          # 4.0
+print(f'floor division of {x} and {y}: {x // y}')   # 4 - force integer division
+print(f'modulo of {x} and {y}: {x % y}')            # 0    
+print(f'power of {x} and {y}: {x ** y}')            # 64
