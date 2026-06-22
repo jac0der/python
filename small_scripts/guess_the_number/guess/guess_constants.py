cf1059489899c2ec1225e1ae96603cc9f43e9622
jacoder <walkerkler@gmail.com>
2026-06-22 06:55:01 -0500

    Add enum constants to represent user guessed answer. completed the compare function.

--- small_scripts/guess_the_number/guess/guess_constants.py
+++ small_scripts/guess_the_number/guess/guess_constants.py
@@ -1,1 +1,7 @@
-EXIT_MESSAGE = "Goodbye!"
+EXIT_MESSAGE:str  = "Goodbye!"
+GET_NUMBER_WARNING:str  = "Invalid input entered." + "\n" + "Please enter an integer value."
+USER_NUMBER_MSG:str  = "User entered number: {0} \n"
+RANDOM_NUMBER_MSG:str = "Random number generated is: {0} \n"
+START_INT:int = 1
+END_INT:int = 100
+TRIES:int = 4
---
+++ small_scripts/guess_the_number/guess/guess_result_enum.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+class GuessResult(IntEnum):
+    LOW = -1
+    CORRECT = 0
+    HIGH = 1
